b1b9a341f3fed18f3fed4f3e833c95a4b32551ee
TTUDhanushka <dhanushka.liyanage@ttu.ee>
2019-02-14 00:07:39 +0200

    t

---
+++ ImuNoiseModels/GaussianNoise.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImuNoiseModels
+{
+    public class GaussianNoise
+    {
+        public double GenerateGaussianNoise()
+        {
+            Random rnd = new Random(1);
+
+            double x1 = 1 - rnd.NextDouble();
+            double x2 = 1 - rnd.NextDouble();
+
+            double y1 = Math.Sqrt(-2.0 * Math.Log(x1) * Math.Cos(2.0 * Math.PI * x2));
+
+            return y1;
+        }
+        
+    }
+}
--- NoiseModels/Program.cs
+++ NoiseModels/Program.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using ImuNoiseModels;
 
 namespace NoiseModels
 {
     class Program
     {
+        private static Thread backGroundThread;
+
         static void Main(string[] args)
         {
+
+
+            backGroundThread = new Thread(DoWork);
+
+            backGroundThread.Start();
+        }
+
+        private static void DoWork()
+        {
+            GaussianNoise gaussianNoise = new GaussianNoise();
+
+
+
+            while (true)
+            {
+                double gauss =  gaussianNoise.GenerateGaussianNoise();
+            }
         }
     }
 }
